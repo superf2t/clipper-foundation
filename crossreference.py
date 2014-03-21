d35da78d4790ea1f90f99801d0b03e98a902e01b
Jonathan Goldman <jonathan@unicyclelabs.com>
2014-03-21 16:13:51 -0700

    Fix handling of certain hotels.com crossreference queries for entities with 'hotel' in the name.

--- crossreference.py
+++ crossreference.py
@@ -7,8 +7,13 @@ HOTELSDOTCOM_AUTOCOMPLETE_URL = ('''http://lookup.hotels.com/1/suggest/v1/json?'
 
 def find_hotelsdotcom_url(hotel_name):
     hotel_name = hotel_name.lower().strip().replace(',', '')
-    if hotel_name.endswith('hotel'):
+    url = _hotelsdotcom_helper(hotel_name)
+    if not url and hotel_name.endswith('hotel'):
         hotel_name = hotel_name[:-5].strip()
+        url = _hotelsdotcom_helper(hotel_name)
+    return url
+
+def _hotelsdotcom_helper(hotel_name):
     query_url = HOTELSDOTCOM_AUTOCOMPLETE_URL % hotel_name.replace(' ', '+')
     response = urllib2.urlopen(query_url).read()
     relevant_json = response[response.find('{'):response.rfind('}')+1]
@@ -22,5 +27,12 @@ def find_hotelsdotcom_url(hotel_name):
     return None
 
 if __name__ == '__main__':
-    print find_hotelsdotcom_url('Mandarin Oriental, Kuala Lumpur Hotel')
-    print find_hotelsdotcom_url('westin st francis san francisco')
+    url = find_hotelsdotcom_url('Mandarin Oriental, Kuala Lumpur Hotel')
+    print url
+    assert '128775' in url
+    url = find_hotelsdotcom_url('westin st francis san francisco')
+    print url
+    assert '114219' in url
+    url = find_hotelsdotcom_url('W San Francisco Hotel')
+    print url
+    assert '145299' in url
