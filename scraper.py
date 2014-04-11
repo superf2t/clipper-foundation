e0bd4c3ede1021d5c2b358d0344c9610547e9ea4
Matthew Liu <matt@unicyclelabs.com>
2014-04-10 18:17:34 -0700

    Lonely Planet scraper

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ from lxml import etree
 
 import crossreference
 import geocode
+import google_places
 import values
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36'
@@ -569,6 +570,103 @@ HiltonScraper.HANDLEABLE_URL_PATTERNS = urlpatterns(
     ('^http(s)?://secure(\d)?\.hilton\.com/.*$', HiltonScraper.expand_reservation_page, REQUIRES_PAGE_SOURCE))
 
 
+class LonelyPlanetScraper(ScrapedPage):
+    HANDLEABLE_URL_PATTERNS = urlpatterns(
+        '^http(s)?://(www)\.lonelyplanet\.com/.*/.*/hotels/(?!guesthouse|apartments|rated|hostels-and-budget-hotels).*$',
+        '^http(s)?://(www)\.lonelyplanet\.com/.*/.*/shopping/.*$',
+        '^http(s)?://(www)\.lonelyplanet\.com/.*/.*/entertainment-nightlife/.*$',
+        '^http(s)?://(www)\.lonelyplanet\.com/.*/.*/sights/.*$',
+        '^http(s)?://(www)\.lonelyplanet\.com/.*/.*/restaurants/.*$',)
+
+    NAME_XPATH = './/h1'
+
+    def get_address(self):
+        city, country = self.get_city_and_country()
+        if '/hotels/' in self.url:
+            street_and_city_node = self.root.find('.//div[@class="vcard lodging__subtitle"]')
+            street_and_city = tostring(street_and_city_node, True)
+            return street_and_city + ' ' + country
+        else:
+            street_node = self.root.find('.//dl[@class="info-list"]//dd[@class="copy--meta"]//strong')
+            street = tostring(street_node, True)
+            return street + ' ' + city + ' ' + country
+
+    def get_category(self):
+        if '/hotels/' in self.url:
+            return values.Category.LODGING
+        elif '/restaurants/' in self.url:
+            return values.Category.FOOD_AND_DRINK
+        elif '/sights/' in self.url or '/shopping/' in self.url or '/entertainment-nightlife/' in self.url:
+            return values.Category.ATTRACTIONS
+        return None
+
+    def get_sub_category(self):
+        if '/hotels/' in self.url:
+            hotel_type_node = self.root.find('.//div[@class="lodging__subtitle"]')
+            hotel_type = tostring(hotel_type_node, True)
+            if hotel_type == 'Guesthouse':
+                return values.SubCategory.BED_AND_BREAKFAST
+            elif hotel_type == 'Hostel':
+                return values.SubCategory.HOSTEL
+            else:
+                return values.SubCategory.HOTEL
+        elif '/restaurants/' in self.url:
+            return values.Category.RESTAURANT
+        else:
+            return None
+        return None
+
+    def get_rating(self):
+        return ''
+
+    def get_primary_photo(self):
+        try:
+            return self.root.find('.//img[@class="media-gallery__img"]').get('src')
+        except:
+            photo_urls = self.get_photos()
+            return photo_urls[0] if photo_urls else None
+
+    def get_city_and_country(self):
+        country, city = self.url.split("/")[3:5]
+        return city.capitalize(), country.capitalize()
+
+    def lookup_google_place(self):
+        if not hasattr(self, '_google_place'):
+            city, country = self.get_city_and_country()
+            query = '%s %s %s' % (self.get_entity_name(), city, country)
+            place_result = geocode.lookup_place(query)
+            if place_result:
+                place = google_places.lookup_place_by_reference(place_result.get_reference())
+                self._google_place = place.to_entity() if place else None
+            else:
+                self._google_place = None
+
+        return self._google_place
+
+    def get_photos(self):
+        urls = []
+        try:
+            for img in self.root.findall('.//img[@class="media-gallery__img"]'):
+                urls.append(img.get('src'))
+            for img in self.root.findall('.//img[@data-class="media-gallery__img"]'):
+                urls.append(img.get('data-src'))
+        except:
+            pass
+        if not urls:
+            google_place_entity = self.lookup_google_place()
+            if google_place_entity:
+                urls.extend(google_place_entity.photo_urls)
+        return urls
+
+    def get_latlng(self):
+        google_place_entity = self.lookup_google_place()
+        if google_place_entity:
+            return google_place_entity.latlng.to_json_obj() if google_place_entity.latlng else None
+        return None
+
+    def get_location_precision(self):
+        return 'Precise' if self.get_latlng() else 'Imprecise'
+
 def tostring_with_breaks(element):
     raw_html = etree.tostring(element)
     elem = element
@@ -686,6 +784,10 @@ if __name__ == '__main__':
         'http://www3.hilton.com/en/hotels/united-kingdom/the-trafalgar-london-LONTSHI/accommodations/index.html',
         'https://secure3.hilton.com/en_US/hi/reservation/book.htm?execution=e3s1',
         'http://www3.hilton.com/en/hotels/ohio/hilton-akron-fairlawn-CAKHWHF/index.html',
+        'http://www.lonelyplanet.com/spain/barcelona/restaurants/japanese/koy-shunka',
+        'http://www.lonelyplanet.com/spain/barcelona/hotels/hostal-abrevadero',
+        'http://www.lonelyplanet.com/spain/barcelona/sights/religious/la-sagrada-familia',
+        'http://www.lonelyplanet.com/spain/barcelona/entertainment-nightlife/other/la-caseta-del-migdia',
         ):
         scrapers = build_scrapers(url, scraper_page_source.get_page_source(url))
         for scraper in scrapers:
