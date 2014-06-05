02ee5a762b5ac537bb72aedd66aec576d984cc3a
Jonathan Goldman <jonathan@unicyclelabs.com>
2014-06-05 11:04:18 -0700

    Check in new marker svgs.

--- tools/process_svgs.py
+++ tools/process_svgs.py
@@ -5,22 +5,33 @@ import sys
 
 def process_file(infile, fname):
     print '''<script type="text/ng-template" id="%s-icon-template">''' % fname.split('.')[0]
+    active_close_tag = None
     for line in infile:
-        if should_skip(line):
+        line = line.strip()
+        if not active_close_tag:
+            active_close_tag = look_for_skip_tag(line)
+        if should_skip(line) or active_close_tag:
+            if active_close_tag and (active_close_tag in line):
+                active_close_tag = None
             continue
         line = strip_tags(line)
-        print '  ' + line,
-        if '</svg>' in line and not line.endswith('\n'):
-            print
+        print '  ' + line
     print '</script>'
 
 def should_skip(line):
     return line.startswith('<?xml') or line.startswith('<!')
 
-STRIP_ID_RE = re.compile('''id=["'][^"']+["']''')
+def look_for_skip_tag(line):
+    if line.startswith('<g ') and 'display="none"' in line:
+        return '</g>'
+    if '<style' in line:
+        return '</style>'
+    return None
+
+STRIP_TAGS_RE = re.compile('''(id|class)=["'][^"']+["']''')
 
 def strip_tags(line):
-    return STRIP_ID_RE.sub('', line)
+    return STRIP_TAGS_RE.sub('', line)
 
 def main(argv):
     for fname in argv[1:]:
