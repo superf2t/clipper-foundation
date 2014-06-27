b947a39bbdb91eadc5b096d8d0d34deccace536c
Jonathan Goldman <jonathan@unicyclelabs.com>
2014-06-27 12:47:44 -0700

    Infer places by looking at links on the page, if we don't recognize the given url.

--- clip_logic.py
+++ clip_logic.py
@@ -1,6 +1,8 @@
+import re
+import urlparse
+
 import data
 import scraper
-
 import utils
 
 # TODO: Remove url as a param here since it's no longer used.
@@ -14,10 +16,43 @@ def entity_from_scraper(scr, url):
         primary_photo_url=scr.get_primary_photo(), photo_urls=scr.get_photos(),
         source_url=scr.get_url())
 
-def scrape_entities_from_url(url, page_source=None, force_fetch_page=False, max_results=None):
+def scrape_entities_from_url(url, page_source=None, force_fetch_page=False,
+        max_results=None, allow_expansion=True):
     scrapers = scraper.build_scrapers(url, page_source, force_fetch_page)
     scrapers = scrapers[:max_results] if max_results else scrapers
     return utils.parallelize(entity_from_scraper, [(scr, url) for scr in scrapers])
 
 def needs_client_page_source_to_scrape(url):
     return scraper.url_requires_client_page_source(url)
+
+def scrape_entities_from_page_source(url, page_source):
+    if scraper.is_url_handleable(url):
+        return scrape_entities_from_url(url, page_source)
+    else:
+        urls = extract_urls_from_page_source(url, page_source)
+        handleable_urls = set(u for u in urls if scraper.is_url_handleable(u, allow_expansion=False))
+        entity_lists = utils.parallelize(scrape_entities_from_url,
+            [(u, None, True, None, False) for u in handleable_urls])
+        return utils.flatten(entity_lists)
+
+def extract_urls_from_page_source(url, page_source):
+    urls = []
+    tree = scraper.parse_tree_from_string(page_source)
+    urls.extend(extract_all_links_from_anchors(url, tree))
+    urls.extend(extract_all_links_from_text(scraper.tostring(tree.getroot())))
+    return urls
+
+def extract_all_links_from_anchors(url, page_source_tree):
+    urls = []
+    links = page_source_tree.getroot().findall('.//a')
+    for link in links:
+        href = link.get('href')
+        if href and (href.startswith('http') or href.startswith('/')):
+            full_url = urlparse.urljoin(url, href)
+            urls.append(full_url)
+    return urls
+
+TEXT_LINK_RE = re.compile('(?i)http(?:s)?://\S+')
+
+def extract_all_links_from_text(text):
+    return TEXT_LINK_RE.findall(text)
