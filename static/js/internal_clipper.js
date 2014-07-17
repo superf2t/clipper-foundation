7cc0d711e89925b8df23b44795cc02e4526ba7ce
Jonathan Goldman <jonathan@unicyclelabs.com>
2014-07-17 12:34:37 -0700

    Add editing of basic trip plan settings.

--- static/js/internal_clipper.js
+++ static/js/internal_clipper.js
@@ -25,14 +25,81 @@ function InternalClipperRootCtrl($scope, $stateModel, $allTripPlans, $entityServ
         $stateModel.tripPlanModel.resetEntities(response['entities']);
       });
   });
+
+  $scope.openEditTripPlan = function() {
+    $stateModel.state = ClipperState.EDIT_TRIP_PLAN;
+  };
+
+  $scope.closeEditTripPlan = function() {
+    $stateModel.state = ClipperState.SUMMARY;
+  };
 }
 
 function TripPlanPanelCtrl($scope) {
 
 }
 
-function EditTripPlanCtrl($scope) {
+function EditTripPlanCtrl($scope, $stateModel, $tripPlanService) {
+  $scope.editableTripPlan = angular.copy($stateModel.tripPlan);
+  $scope.saving = false;
 
+  $scope.mapState = {map: null};
+  $scope.mapOptions = {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 1,
+    panControl: false,
+    scaleControl: false,
+    scrollwheel: false,
+    streetViewControl: false,
+    mapTypeControl: false
+  };
+
+  $scope.setupMap = function($map) {
+    if (!_.isEmpty($scope.editableTripPlan['location_bounds'])) {
+      $map.fitBounds(gmapsBoundsFromJson($scope.editableTripPlan['location_bounds']));
+    }
+  };
+
+  $scope.addressSelected = function(place) {
+    var geometry = place['geometry'];
+    var location = geometry && geometry['location'];
+    var viewport = geometry && geometry['viewport'];
+    var tripPlan = $scope.editableTripPlan;
+    tripPlan['location_name'] = place['formatted_address'];
+    if (location) {
+      tripPlan['location_latlng'] = {
+        'lat': location.lat(),
+        'lng': location.lng()
+      };
+    }
+    if (viewport) {
+      tripPlan['location_bounds'] = {
+        'southwest': {
+          'lat': viewport.getSouthWest().lat(),
+          'lng': viewport.getSouthWest().lng()
+        },
+        'northeast': {
+          'lat': viewport.getNorthEast().lat(),
+          'lng': viewport.getNorthEast().lng()
+        }
+      }
+    }
+    if (viewport) {
+      $scope.mapState.map.fitBounds(gmapsBoundsFromJson(tripPlan['location_bounds']));
+    } else if (location) {
+      $scope.mapState.map.setCenter(gmapsLatLngFromJson(tripPlan['location_latlng']));
+    }
+  };
+
+  $scope.saveTripPlan = function() {
+    $scope.saving = true;
+    $tripPlanService.editTripPlan($scope.editableTripPlan)
+      .success(function(response) {
+        _.extend($stateModel.tripPlan, response['trip_plans'][0]);
+        $stateModel.state = ClipperState.SUMMARY;
+        $scope.saving = false;
+      });
+  };
 }
 
 function EditEntityCtrl($scope) {
