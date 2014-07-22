676b83bbc3f4bae471930258e360b63ea686daa3
Jonathan Goldman <jonathan@unicyclelabs.com>
2014-07-22 16:38:18 -0700

    Add best effort parsing for Bon Appetit to get images and descriptions.

--- scraping/bon_appetit_guide.py
+++ scraping/bon_appetit_guide.py
@@ -1,9 +1,12 @@
+import json
 import re
 
 from dateutil import parser as date_parser
 from dateutil import tz
+from lxml import etree
 
 import article_parser
+import data
 from scraping import html_parsing
 from scraping.scraped_page import fail_returns_none
 
@@ -28,3 +31,17 @@ class BonAppetitGuide(article_parser.ArticleParser):
             self.root.xpath('.//div[@class="top_meta"]//span[@class="date"]')[0])
         date_text = date_text_line.replace('Published on', '').strip()
         return date_parser.parse(date_text).replace(tzinfo=tz.tzutc())
+
+    def get_raw_entities(self):
+        slideshow_js = self.root.xpath('.//article[@id="slide-content"]/script/text()')[0].strip()
+        slideshow_js = slideshow_js.replace('var Slideshow = ', '').rstrip(';').strip()
+        parsed_js = json.loads(slideshow_js)
+        entities = []
+        for obj in parsed_js['relatedContent']:
+            item = obj['item']
+            img_url = item['body']['photo']['images']['image'][0]['source']
+            desc_html = item['subHeaders']['subHeader']['text']
+            desc_html_no_metadata = re.sub('<em>.*</em>', '', desc_html)
+            desc = html_parsing.tostring(etree.fromstring('<foo>%s</foo>' % desc_html_no_metadata))
+            entities.append(data.Entity(photo_urls=[img_url], description=desc))
+        return entities
