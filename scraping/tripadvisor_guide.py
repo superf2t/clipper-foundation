ea76f8b0ea7bc14f3b7ca79d09e9dc1857d90f35
Jonathan Goldman <jonathan@unicyclelabs.com>
2014-07-24 11:34:36 -0700

    Auto-tag day numbers on tripadvisor guides.

--- scraping/tripadvisor_guide.py
+++ scraping/tripadvisor_guide.py
@@ -1,7 +1,9 @@
 import re
 
 import article_parser
+import data
 from scraping import html_parsing
+from scraping.scraped_page import fail_returns_none
 
 class TripAdvisorGuide(article_parser.ArticleParser):
     URL_REGEX = re.compile('(http://www\.tripadvisor\.com/Guide-.+\.html).*')
@@ -19,6 +21,21 @@ class TripAdvisorGuide(article_parser.ArticleParser):
                 return page_header.replace('Travel Guide for ', '')
         return None
 
+    @fail_returns_none
     def get_description(self):
         return html_parsing.tostring(
             self.root.xpath('.//div[@class="guideDescription"]')[0])
+
+    @fail_returns_none
+    def get_entity_overrides(self):
+        overrides = {}
+        current_day = 0
+        for node in self.root.xpath(
+            './/div[@id="GUIDE_DETAIL"]//div[contains(@class, "guideOverview")]')[0].itersiblings():
+            if node.tag == 'h5':
+                current_day = int(node.text.replace('Day', '').strip())
+            elif node.tag == 'div':
+                rel_source_url = node.xpath('div[@class="guideItemInfo"]//a[@class="titleLink"]/@href')[0]
+                overrides[self.absolute_url(rel_source_url)] = data.Entity(
+                    tags=[data.Tag(text='Day %d' % current_day)])
+        return overrides
