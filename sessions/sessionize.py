e476bf5fa239bcae74c9af7b2972cf8fc76ddfaf
Jonathan Goldman <jonathan@unicyclelabs.com>
2014-08-15 18:08:52 -0700

    Add interactions to the session.

--- sessions/sessionize.py
+++ sessions/sessionize.py
@@ -1,4 +1,5 @@
 import datetime
+import operator
 import re
 
 from dateutil import parser as date_parser
@@ -13,6 +14,13 @@ class SessionEvent(object):
         self.request = request
         self.interaction = interaction
         self.trip_plan = trip_plan
+
+    @property
+    def timestamp(self):
+        if self.request:
+            return self.request.timestamp
+        else:
+            return self.interaction.timestamp
 
 TRIP_PLAN_ID_RE = re.compile('/guide/(\d+)')
 
@@ -37,6 +45,13 @@ def expand_session(visitor_id, date_str=None):
     trip_plans = data.load_trip_plans_by_ids(trip_plan_ids)
     trip_plans_by_id = dict((t.trip_plan_id, t) for t in trip_plans)
 
+    int_cls = request_logging.FrontendInteractionLogRecord
+    interaction_records = int_cls.query \
+        .filter_by(visitor_id=visitor_id) \
+        .filter(int_cls.timestamp >= date).filter(int_cls.timestamp < next_day) \
+        .order_by(int_cls.timestamp) \
+        .all()
+
     session_events = []
     for record in request_records:
         event = SessionEvent(request=record)
@@ -44,17 +59,26 @@ def expand_session(visitor_id, date_str=None):
             trip_plan_id = int(TRIP_PLAN_ID_RE.match(record.url).group(1))
             event.trip_plan = trip_plans_by_id.get(trip_plan_id)
         session_events.append(event)
+    for record in interaction_records:
+        event = SessionEvent(interaction=record)
+        session_events.append(event)
+    session_events = sorted(session_events, key=operator.attrgetter('timestamp'))
 
     for event in session_events:
-        if event.trip_plan:
-            format = '%(url)s - %(time)s (%(trip_plan_name)s)'
+        timestamp = event.timestamp.astimezone(PACIFIC_TIME).strftime('%X')
+        if event.request:
+            if event.trip_plan:
+                format = '%(url)s (%(trip_plan_name)s)\t\t\t\t%(time)s'
+            else:
+                format = '%(url)s\t\t\t\t%(time)s'
+            print format % {
+                'url': event.request.url,
+                'time': timestamp,
+                'trip_plan_name': event.trip_plan.name if event.trip_plan else None,
+                }
         else:
-            format = '%(url)s - %(time)s'
-        print format % {
-            'url': event.request.url,
-            'time': event.request.timestamp.astimezone(PACIFIC_TIME).strftime('%X'),
-            'trip_plan_name': event.trip_plan.name if event.trip_plan else None,
-            }
+            print '\t%s, %s, %s\t\t\t\t%s' % (event.interaction.event_name, event.interaction.event_location,
+                event.interaction.event_value, timestamp)
 
 
 if __name__ == '__main__':
